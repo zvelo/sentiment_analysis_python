c28ae64b97e158bd2359313cb31f3e4c92dd8145
Arnold Benedict Tan Casis <atancasis@zvelo.com>
2013-11-26 10:49:45 -0800

    Add method returning sentiment rating for a given phrase.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,14 +8,6 @@ POLARITY_DATA_DIR = os.path.join('polarityData', 'rt-polaritydata')
 RT_POLARITY_POS_FILE = os.path.join(POLARITY_DATA_DIR, 'rt-polarity-pos.txt')
 RT_POLARITY_NEG_FILE = os.path.join(POLARITY_DATA_DIR, 'rt-polarity-neg.txt')
 
-def main():
-    #tries using all words as the feature selection mechanism
-    print 'using all words as features'
-    evaluate_features(make_full_dict)
-
-    #finds word scores
-    word_scores = create_word_scores()
-
 #this function takes a feature selection mechanism and returns its performance in a variety of metrics
 def evaluate_features(feature_select):
 	posFeatures = []
@@ -33,77 +25,16 @@ def evaluate_features(feature_select):
 			negWords = [feature_select(negWords), 'neg']
 			negFeatures.append(negWords)
 
-	
-	#selects 3/4 of the features to be used for training and 1/4 to be used for testing
-	posCutoff = int(math.floor(len(posFeatures)*3/4))
-	negCutoff = int(math.floor(len(negFeatures)*3/4))
-	trainFeatures = posFeatures[:posCutoff] + negFeatures[:negCutoff]
-	testFeatures = posFeatures[posCutoff:] + negFeatures[negCutoff:]
-
-	#trains a Naive Bayes Classifier
-	classifier = NaiveBayesClassifier.train(trainFeatures)	
-
-	#initiates referenceSets and testSets
-	referenceSets = collections.defaultdict(set)
-	testSets = collections.defaultdict(set)	
-
-	#puts correctly labeled sentences in referenceSets and the predictively labeled version in testsets
-	for i, (features, label) in enumerate(testFeatures):
-		referenceSets[label].add(i)
-		predicted = classifier.classify(features)
-		testSets[predicted].add(i)	
-
-	#prints metrics to show how well the feature selection did
-	print 'train on %d instances, test on %d instances' % (len(trainFeatures), len(testFeatures))
-	print 'accuracy:', nltk.classify.util.accuracy(classifier, testFeatures)
-	print 'pos precision:', nltk.metrics.precision(referenceSets['pos'], testSets['pos'])
-	print 'pos recall:', nltk.metrics.recall(referenceSets['pos'], testSets['pos'])
-	print 'neg precision:', nltk.metrics.precision(referenceSets['neg'], testSets['neg'])
-	print 'neg recall:', nltk.metrics.recall(referenceSets['neg'], testSets['neg'])
-	classifier.show_most_informative_features(10)
+	trainFeatures = posFeatures + negFeatures
+	global CLASSIFIER
+	CLASSIFIER = NaiveBayesClassifier.train(trainFeatures)	
 
 #creates a feature selection mechanism that uses all words
 def make_full_dict(words):
 	return dict([(word, True) for word in words])
 
-#scores words based on chi-squared test to show information gain (http://streamhacker.com/2010/06/16/text-classification-sentiment-analysis-eliminate-low-information-features/)
-def create_word_scores():
-	#creates lists of all positive and negative words
-	posWords = []
-	negWords = []
-	with open(RT_POLARITY_POS_FILE, 'r') as posSentences:
-		for i in posSentences:
-			posWord = re.findall(r"[\w']+|[.,!?;]", i.rstrip())
-			posWords.append(posWord)
-	with open(RT_POLARITY_NEG_FILE, 'r') as negSentences:
-		for i in negSentences:
-			negWord = re.findall(r"[\w']+|[.,!?;]", i.rstrip())
-			negWords.append(negWord)
-	posWords = list(itertools.chain(*posWords))
-	negWords = list(itertools.chain(*negWords))
+# returns sentiment rating for a given phrase
+def sentiment(phrase):
+    return CLASSIFIER.classify(make_full_dict(phrase))
 
-	#build frequency distibution of all words and then frequency distributions of words within positive and negative labels
-	word_fd = FreqDist()
-	cond_word_fd = ConditionalFreqDist()
-	for word in posWords:
-		word_fd.inc(word.lower())
-		cond_word_fd['pos'].inc(word.lower())
-	for word in negWords:
-		word_fd.inc(word.lower())
-		cond_word_fd['neg'].inc(word.lower())
-
-	#finds the number of positive and negative words, as well as the total number of words
-	pos_word_count = cond_word_fd['pos'].N()
-	neg_word_count = cond_word_fd['neg'].N()
-	total_word_count = pos_word_count + neg_word_count
-
-	#builds dictionary of word scores based on chi-squared test
-	word_scores = {}
-	for word, freq in word_fd.iteritems():
-		pos_score = BigramAssocMeasures.chi_sq(cond_word_fd['pos'][word], (freq, pos_word_count), total_word_count)
-		neg_score = BigramAssocMeasures.chi_sq(cond_word_fd['neg'][word], (freq, neg_word_count), total_word_count)
-		word_scores[word] = pos_score + neg_score
-
-	return word_scores
-
-main()
+evaluate_features(make_full_dict)
