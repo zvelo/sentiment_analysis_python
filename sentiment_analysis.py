951c2399ba79166113bc93d0a410325d2e8e2ec1
Arnold Benedict Tan Casis <atancasis@zvelo.com>
2013-11-26 10:22:13 -0800

    Remove subroutines related to selection as the intent for modification will be utilizing all features.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,11 +4,17 @@ from nltk.classify import NaiveBayesClassifier
 from nltk.metrics import BigramAssocMeasures
 from nltk.probability import FreqDist, ConditionalFreqDist
 
-
 POLARITY_DATA_DIR = os.path.join('polarityData', 'rt-polaritydata')
 RT_POLARITY_POS_FILE = os.path.join(POLARITY_DATA_DIR, 'rt-polarity-pos.txt')
 RT_POLARITY_NEG_FILE = os.path.join(POLARITY_DATA_DIR, 'rt-polarity-neg.txt')
 
+def main():
+    #tries using all words as the feature selection mechanism
+    print 'using all words as features'
+    evaluate_features(make_full_dict)
+
+    #finds word scores
+    word_scores = create_word_scores()
 
 #this function takes a feature selection mechanism and returns its performance in a variety of metrics
 def evaluate_features(feature_select):
@@ -60,10 +66,6 @@ def evaluate_features(feature_select):
 def make_full_dict(words):
 	return dict([(word, True) for word in words])
 
-#tries using all words as the feature selection mechanism
-print 'using all words as features'
-evaluate_features(make_full_dict)
-
 #scores words based on chi-squared test to show information gain (http://streamhacker.com/2010/06/16/text-classification-sentiment-analysis-eliminate-low-information-features/)
 def create_word_scores():
 	#creates lists of all positive and negative words
@@ -104,23 +106,4 @@ def create_word_scores():
 
 	return word_scores
 
-#finds word scores
-word_scores = create_word_scores()
-
-#finds the best 'number' words based on word scores
-def find_best_words(word_scores, number):
-	best_vals = sorted(word_scores.iteritems(), key=lambda (w, s): s, reverse=True)[:number]
-	best_words = set([w for w, s in best_vals])
-	return best_words
-
-#creates feature selection mechanism that only uses best words
-def best_word_features(words):
-	return dict([(word, True) for word in words if word in best_words])
-
-#numbers of features to select
-numbers_to_test = [10, 100, 1000, 10000, 15000]
-#tries the best_word_features mechanism with each of the numbers_to_test of features
-for num in numbers_to_test:
-	print 'evaluating best %d word features' % (num)
-	best_words = find_best_words(word_scores, num)
-	evaluate_features(best_word_features)
+main()
